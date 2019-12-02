80f4b2b20d9bcb108f3b243d8dab4a6ab07e18fe
carlesRa <juan.ramos.a@ies1xabia.org>
2019-12-02 18:45:54 +0100

    first commit

---
+++ app/src/main/java/com/carlesramos/pm_intenttest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.carlesramos.pm_intenttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
